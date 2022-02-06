2b86b9ccf9ef2e87bbb25013c3e048601a1434f3
Gabriel Marinho <gdcmarinho@gmail.com>
2022-02-05 21:25:15 -0300

    add install shell script

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Update the operation system
+sudo apt-get update
+
+# Install essential software
+sudo apt-get install git zsh ruby
+
+# Install OhMyZsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
