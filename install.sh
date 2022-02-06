f11b7655fa18fecf992e5e9c08f9b8eafcaa3874
Gabriel Marinho <gdcmarinho@gmail.com>
2022-02-05 23:18:36 -0300

    add git configuration steps

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
+#!/bin/sh
+
 # Update the operation system
 sudo apt-get update
+
+echo "What is your fullname (Git configuration)?"
+read USER_FULLNAME
+
+echo "What is your email address (Git configuration)?"
+read USER_EMAIL
+
+git config --global user.name $USER_FULLNAME
+git config --global user.email $USER_EMAIL
 
 # Install essential software
 sudo apt-get install git zsh ruby
