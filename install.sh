40406a4b2d21918768fec7bc9a1d6104fcb23f89
Gabriel Marinho <gdcmarinho@gmail.com>
2022-02-06 17:40:58 -0300

    add printed messages to improve process readability

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Update the operation system
+echo "Updating the operation system..."
 sudo apt-get update
 
 echo "What is your fullname (Git configuration)?"
@@ -9,13 +10,16 @@ read USER_FULLNAME
 echo "What is your email address (Git configuration)?"
 read USER_EMAIL
 
+echo "Starting Git configuration with provided data..."
 git config --global user.name $USER_FULLNAME
 git config --global user.email $USER_EMAIL
 
 # Install essential software
+echo "Installing essential software to environment"
 sudo apt-get install git zsh ruby
 
 # Install OhMyZsh
+echo "Installing OhMyZsh to improve terminal experience"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "✅ Installation and configuration completed successfully"
