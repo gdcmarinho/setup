5af5757ae58cb5234151ddc5cdce57f183bc5acf
varun <102137626+VARUNREDDYVADDEPALLI@users.noreply.github.com>
2022-09-20 00:21:06 +0530

    Add files via upload

---
+++ open file projet.py
@@ -0,0 +1,8 @@
+import re
+
+##f = open(r"C:\Users\Kound\OneDrive\Desktop\Saduvu\ITU\Fall 2022\Python\Visual basic\RegularExpression.txt","r")
+##print(f.read())
+
+with open(r'D:\itu project vs\varun par.txt') as f:
+        lines = f.readlines()
+        print(lines)
